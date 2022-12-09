aeffc615abaeb6cbd895652ecfd1b32c4dbbdb05
Mohed Alrahbi <mccdama@gmail.com>
2022-12-09 15:40:23 +0100

    Board out of Squares

--- src/index.js
+++ src/index.js
@@ -14,6 +14,39 @@ class Square extends React.Component {
     }
 }
 
+// The Board renders 9 squares.
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare("X")}
+                    {this.renderSquare("X")}
+                    {this.renderSquare("X")}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(4)}
+                    {this.renderSquare(6)}
+                    {this.renderSquare(9)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare("Y")}
+                    {this.renderSquare("Y")}
+                    {this.renderSquare("Y")}
+                </div>
+            </div>
+        );
+    }
+}
+
+
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
