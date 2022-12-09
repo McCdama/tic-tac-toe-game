b2789904f4e133039c7c9091b82d9590b33d3958
Mohed Alrahbi <mccdama@gmail.com>
2022-12-09 15:42:21 +0100

    Passing real numbers

--- src/index.js
+++ src/index.js
@@ -27,19 +27,19 @@ class Board extends React.Component {
             <div>
                 <div className="status">{status}</div>
                 <div className="board-row">
-                    {this.renderSquare("X")}
-                    {this.renderSquare("X")}
-                    {this.renderSquare("X")}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                    {this.renderSquare(3)}
                 </div>
                 <div className="board-row">
                     {this.renderSquare(4)}
+                    {this.renderSquare(5)}
                     {this.renderSquare(6)}
-                    {this.renderSquare(9)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare("Y")}
-                    {this.renderSquare("Y")}
-                    {this.renderSquare("Y")}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                    {this.renderSquare(9)}
                 </div>
             </div>
         );
