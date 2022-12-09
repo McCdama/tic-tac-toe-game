6e80d773047db42eee5308adf23c35811340ccb6
Mohed Alrahbi <mccdama@gmail.com>
2022-12-09 16:42:00 +0100

    Update handle click

--- src/index.js
+++ src/index.js
@@ -31,6 +31,9 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         // update the source
         this.setState({
