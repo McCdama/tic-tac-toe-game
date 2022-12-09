b0a03847530ed9a6145e026ce3f5baa0d0dacc5b
Mohed Alrahbi <mccdama@gmail.com>
2022-12-09 16:35:58 +0100

    Calculate Winner

--- src/index.js
+++ src/index.js
@@ -89,6 +89,28 @@ class Game extends React.Component {
     }
 }
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 1, 2],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let index = 0; index < lines.length; index++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
