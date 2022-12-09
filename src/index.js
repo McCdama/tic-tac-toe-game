3705255caf78279f315603de9a62eaf6d8b4d854
Mohed Alrahbi <mccdama@gmail.com>
2022-12-09 15:39:43 +0100

    Square Comp.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+// The Square component renders a single <button> 
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+
+            </button>
+        );
+    }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
